6a056eeeeb0eda8080b9c1cf2ed028f33d2d8cbc
sickelap <sickelap@pm.me>
2024-04-22 13:05:30 +0100

    initial commit

---
+++ src/main/java/com/example/sbjconvert/SbjconvertApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbjconvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbjconvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbjconvertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbjconvert/SbjconvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbjconvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbjconvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
