638168bae8b44259155b6e4fded658cca93e1f82
sickelap <sickelap@pm.me>
2024-04-22 13:06:45 +0100

    rename aplication

--- src/main/java/com/example/sbjconvert/Application.java
+++ src/main/java/com/example/sbjconvert/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbjconvertApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbjconvertApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
